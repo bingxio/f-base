e74e19f4529d3220df3c875d4aaccc781ccf8bfb
Turaiiao <3106740988@qq.com>
2021-05-28 14:03:48 +0800

    Update.

--- ast.go
+++ ast.go
@@ -5,90 +5,90 @@ package main
 import "fmt"
 
 const (
-    Se = iota // INSERT
-    Ge        // SELECT
-    Up        // UPDATE
-    De        // DELETE
-    Er        // ERROR
+	Se = iota // INSERT
+	Ge        // SELECT
+	Up        // UPDATE
+	De        // DELETE
+	Er        // ERROR
 )
 
 // SE ? *<E>
 // GE ? | <F> | -> <T>
-// UP ? <P> <E> [<T>]
-// DE ? <P> [<E>]
+// UP ? <P> <N> [<V>]
+// DE ? <P> [<V>]
 type Ast interface {
-    Stringer() string
-    Kind() int
+	Stringer() string
+	Kind() int
 }
 
 // INSERT
 type SeExpr struct {
-    Table  Token
-    Fields []Token
+	Table  Token
+	Fields []Token
 }
 
 func (s SeExpr) Stringer() string {
-    ele := func(l []Token) string {
-        lit := "["
-        for i := 0; i < len(l); i++ {
-            lit += l[i].Literal
+	ele := func(l []Token) string {
+		lit := "["
+		for i := 0; i < len(l); i++ {
+			lit += l[i].Literal
 
-            if i+1 != len(l) {
-                lit += ", "
-            }
-        }
-        lit += "]"
-        return lit
-    }
-    return fmt.Sprintf("SeExpr \n\tTable=%s \n\tFields=%s",
-        s.Table.Stringer(), ele(s.Fields))
+			if i+1 != len(l) {
+				lit += ", "
+			}
+		}
+		lit += "]"
+		return lit
+	}
+	return fmt.Sprintf("SeExpr \n\tTable=%s \n\tFields=%s",
+		s.Table.Stringer(), ele(s.Fields))
 }
 
 func (s SeExpr) Kind() int { return Se }
 
 // SELECT
 type GeExpr struct {
-    Table Token
-    From  Token
-    To    Token
+	Table Token
+	From  Token
+	To    Token
 }
 
 func (g GeExpr) Stringer() string {
-    return fmt.Sprintf("GeExpr \n\tTable=%s \n\tFrom=%s \n\tTo=%s",
-        g.Table.Stringer(), g.From.Stringer(), g.To.Stringer())
+	return fmt.Sprintf("GeExpr \n\tTable=%s \n\tFrom=%s \n\tTo=%s",
+		g.Table.Stringer(), g.From.Stringer(), g.To.Stringer())
 }
 
 func (g GeExpr) Kind() int { return Ge }
 
 // UPDATE
 type UpExpr struct {
-    Table Token
-    Pos   Token
-    New   Token
-    Ver   Token
+	Table Token
+	Pos   Token
+	New   Token
+	Ver   Token
 }
 
 func (u UpExpr) Stringer() string {
-    return fmt.Sprintf(
-        "UpExpr \n\tTable=%s \n\tPos=%s \n\tNew=%s \n\tVer=%s",
-        u.Table.Stringer(),
-        u.Pos.Stringer(),
-        u.New.Stringer(),
-        u.Ver.Stringer())
+	return fmt.Sprintf(
+		"UpExpr \n\tTable=%s \n\tPos=%s \n\tNew=%s \n\tVer=%s",
+		u.Table.Stringer(),
+		u.Pos.Stringer(),
+		u.New.Stringer(),
+		u.Ver.Stringer())
 }
 
 func (u UpExpr) Kind() int { return Up }
 
 // DELETE
 type DeExpr struct {
-    Table Token
-    Pos   Token
-    Ver   Token
+	Table Token
+	Pos   Token
+	Ver   Token
 }
 
 func (d DeExpr) Stringer() string {
-    return fmt.Sprintf("DeExpr \n\tTable=%s \n\tPos=%s \n\tVer=%s",
-        d.Table.Stringer(), d.Pos.Stringer(), d.Ver.Stringer())
+	return fmt.Sprintf("DeExpr \n\tTable=%s \n\tPos=%s \n\tVer=%s",
+		d.Table.Stringer(), d.Pos.Stringer(), d.Ver.Stringer())
 }
 
 func (d DeExpr) Kind() int { return De }
--- main.go
+++ main.go
@@ -3,49 +3,95 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
 )
 
 const Message = "F-Base v0.0.1 * MADE AT May 26 2021 18:06:35"
 
+var (
+	DbPath = ""     // Database
+	DbFile *os.File // Db File
+	DbFlag = os.O_APPEND | os.O_CREATE | os.O_WRONLY
+)
+
 func main() {
-    fmt.Println(Message)
+	flag.Parse()
+	arg := flag.Args()
 
-    buf := bufio.NewReader(os.Stdin)
-    for {
-        fmt.Print("> ")
+	if len(arg) < 1 {
+		fmt.Fprintln(os.Stderr, "LOST DATABASE SPECIFIED")
+		return
+	}
+	DbPath = arg[0]
+	f, err := os.OpenFile(DbPath, DbFlag, 0644)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
+	DbFile = f
+	repl()
+}
 
-        l, _, err := buf.ReadLine()
-        if err != nil {
-            panic(err)
-        }
-        line := string(l)
-        // Empty line
-        if len(line) == 0 {
-            continue
-        }
-        // All empty characters
-        j := 0
-        for i := 0; i < len(line); i++ {
-            if line[i] == ' ' || line[i] == '\t' {
-                j++
-            }
-        }
-        if j == len(line) {
-            continue
-        }
-        // Eval
-        eval(line)
-    }
+func repl() {
+	fmt.Println(Message)
+	fmt.Printf("DB: '%s' %s\n", DbPath, DbInfo())
+
+	buf := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+
+		l, _, err := buf.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		line := string(l)
+		if len(line) == 0 {
+			continue
+		}
+		j := 0
+		for i := 0; i < len(line); i++ {
+			if line[i] == ' ' || line[i] == '\t' {
+				j++
+			}
+		}
+		if j == len(line) {
+			continue
+		}
+		if line == "help" {
+			usage()
+		} else if line == "exit" || line == "quit" {
+			fmt.Println("bye")
+			break
+		} else {
+			eval(line)
+		}
+	}
 }
 
 func eval(src string) {
-    // fmt.Println("Eval: ", src)
-    ast := NewLex(src).parse()
-    if ast.Kind() == Er {
-        return
-    }
-    fmt.Println(ast.Stringer())
+	ast := NewLex(src).parse()
+	if ast.Kind() == Er {
+		return
+	}
+	fmt.Println(ast.Stringer())
+}
+
+func usage() {
+	fmt.Println(
+		`Usage: 
+	SE ? *<E>
+	GE ? | <F> | -> <T>
+	UP ? <P> <N> [<V>]
+	DE ? <P> [<V>]`)
+}
+
+func DbInfo() string {
+	stat, err := DbFile.Stat()
+	if err != nil {
+		return err.Error()
+	}
+	return fmt.Sprint(stat.Size())
 }
